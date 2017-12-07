e8a8570254be0e9178dd8d7d5de68ed434730c1d
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-07 15:04:09 -0800

    - Return null from render when no icon is found - Add negative test case for not found icons - Add listeners to logger to use in test

--- src/components/__tests__/FontAwesomeIcon.test.js
+++ src/components/__tests__/FontAwesomeIcon.test.js
@@ -2,6 +2,7 @@ import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '../FontAwesomeIcon'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import {listen, clearListeners} from "../../logger";
 
 const faCoffee = {
   prefix: 'fas',
@@ -37,6 +38,10 @@ function mount (props = {}) {
   return component.toJSON()
 }
 
+afterEach(() => {
+  clearListeners();
+});
+
 test('using pack and name', () => {
   const vm = mount({ icon: ['fas', 'coffee'], style: { backgroundColor: 'white' } })
 
@@ -52,6 +57,16 @@ test('using pack common names', () => {
 
   expect(vm.type).toBe('svg')
   expect(vm.props.className.includes('fa-coffee')).toBeTruthy()
+})
+
+test('using pack common names not added to library', () => {
+  const loggerCallback = jest.fn()
+  const loggerCallbackKey = `${Math.random()}-${Math.random()}-${Math.random()}`
+  listen(loggerCallbackKey, loggerCallback)
+  const vm = mount({ icon: 'spinner' })
+  expect(vm).toBeNull()
+  expect(loggerCallback.mock.calls.length).toBe(1)
+  expect(loggerCallback.mock.calls[0][0]).toEqual(expect.stringContaining('Could not find icon'))
 })
 
 test('using icon', () => {
--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,37 @@
 let PRODUCTION = false
+let TEST = false
+let listeners = {};
 
 try {
   PRODUCTION = process.env.NODE_ENV === 'production'
 } catch (e) { }
 
+try {
+  TEST = process.env.NODE_ENV === 'test'
+} catch (e) { }
+
+export function listen(key, listener){
+  if(!TEST && !PRODUCTION && console && typeof console.error === 'function'){
+    console.error('listen() is only for use in test')
+  }
+  else {
+    listeners[key] = listener
+  }
+}
+export function clearListeners(){
+  if(!TEST && !PRODUCTION && console && typeof console.error === 'function'){
+    console.error('clearListeners() is only for use in test')
+  }
+  else {
+    listeners = {};
+  }
+}
+
 export default function (...args) {
-  if (!PRODUCTION && console && typeof console.error === 'function') {
+  if(TEST) {
+    Object.keys(listeners).forEach(function(key){listeners[key](...args)});
+  }
+  else if (!PRODUCTION && console && typeof console.error === 'function') {
     console.error(...args)
   }
 }
