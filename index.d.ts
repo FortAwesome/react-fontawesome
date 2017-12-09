041cb10c18ef0897a0e77ff04ede82976c385e2b
Andrew Durber <aidurber@gmail.com>
2017-12-09 15:10:59 +0000

    Export enums for Flip, Pull, Rotation, Size. Use namespace instead of module.

--- index.d.ts
+++ index.d.ts
@@ -1,52 +1,135 @@
-declare module '@fortawesome/react-fontawesome' {
-  export type FontAwesomeIconMask = Object | any[] | string;
-
-  export type FontAwesomeIconFlip = 'horizontal' | 'vertical' | 'both';
-
-  export type FontAwesomeIconIcon = Object | any[] | string;
-
-  export type FontAwesomeIconPull = 'right' | 'left';
-
-  export type FontAwesomeIconRotation = 90 | 180 | 270;
-
-  export type FontAwesomeIconSize =
-    | 'lg'
-    | 'xs'
-    | 'sm'
-    | '1x'
-    | '2x'
-    | '3x'
-    | '4x'
-    | '5x'
-    | '6x'
-    | '7x'
-    | '8x'
-    | '9x'
-    | '10x';
-
-  export type FontAwesomeIconSymbol = boolean | string;
-
-  export type FontAwesomeIconTransform = string | Object;
-
-  export interface FontAwesomeIconProps {
-    border?: boolean;
-    className?: string;
-    mask?: FontAwesomeIconMask;
-    fixedWidth?: boolean;
-    flip?: FontAwesomeIconFlip;
-    icon?: FontAwesomeIconIcon;
-    listItem?: boolean;
-    pull?: FontAwesomeIconPull;
-    pulse?: boolean;
-    name?: string;
-    rotation?: FontAwesomeIconRotation;
-    size?: FontAwesomeIconSize;
-    spin?: boolean;
-    symbol?: FontAwesomeIconSymbol;
-    transform?: FontAwesomeIconTransform;
+import * as React from 'react';
+export = FontAwesomeIcon;
+export as namespace FontAwesomeIcon;
+declare namespace FontAwesomeIcon {
+  /**
+   *
+   */
+  export enum Flip {
+    Horizontal = 'horizontal',
+    Vertical = 'vertical',
+    Both = 'both',
   }
-
-  export default function FontAwesomeIcon(
-    props: FontAwesomeIconProps,
-  ): JSX.Element;
+  /**
+   *
+   */
+  export enum Size {
+    Large = 'lg',
+    ExtraSmall = 'xs',
+    Small = 'sm',
+    OneX = '1x',
+    TwoX = '2x',
+    ThreeX = '3x',
+    FourX = '4x',
+    FiveX = '5x',
+    SixX = '6x',
+    SevenX = '7x',
+    EightX = '8x',
+    NineX = '9x',
+    TenX = '10x',
+  }
+  /**
+   *
+   */
+  export enum Pull {
+    Right = 'right',
+    Left = 'left',
+  }
+  /**
+   *
+   */
+  export enum Rotation {
+    NinetyDegrees = 90,
+    OneHundredEightyDegrees = 180,
+    TwoHundredSeventyDegrees = 270,
+  }
+  /**
+   *
+   */
+  export type RotationType = 90 | 180 | 270;
+  /**
+   *
+   */
+  export type Symbol = boolean | string;
+  /**
+   *
+   */
+  export type Mask = object | any[] | string;
+  /**
+   *
+   */
+  export type Icon = object | any[] | string;
+  /**
+   *
+   */
+  export type Transform = string | object;
+  /**
+   *
+   */
+  export interface Props {
+    /**
+     *
+     */
+    border?: boolean;
+    /**
+     *
+     */
+    className?: string;
+    /**
+     *
+     */
+    mask?: Mask;
+    /**
+     *
+     */
+    fixedWidth?: boolean;
+    /**
+     *
+     */
+    flip?: Flip | string;
+    /**
+     *
+     */
+    icon?: Icon;
+    /**
+     *
+     */
+    listItem?: boolean;
+    /**
+     *
+     */
+    pull?: Pull | string;
+    /**
+     *
+     */
+    pulse?: boolean;
+    /**
+     *
+     */
+    name?: string;
+    /**
+     *
+     */
+    rotation?: Rotation | RotationType;
+    /**
+     *
+     */
+    size?: Size | string;
+    /**
+     *
+     */
+    spin?: boolean;
+    /**
+     *
+     */
+    symbol?: Symbol;
+    /**
+     *
+     */
+    transform?: Transform;
+  }
+  /**
+   *
+   */
+  export function Icon(props: Props): JSX.Element;
 }
