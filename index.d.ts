ae3a0ea76e46d50086a8dea498c3b242b5ff554c
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-29 14:43:23 -0800

    reformat with prettier

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,29 @@
 /// <reference types="react" />
-import { CSSProperties } from 'react';
+import { CSSProperties } from "react";
 import {
-    Transform,
-    IconProp,
-    FlipProp,
-    SizeProp,
-    PullProp,
-    RotateProp
-} from '@fortawesome/fontawesome';
+  Transform,
+  IconProp,
+  FlipProp,
+  SizeProp,
+  PullProp,
+  RotateProp
+} from "@fortawesome/fontawesome";
 
 export interface Props {
-    icon: IconProp;
-    mask?: IconProp;
-    className?: string;
-    spin?: boolean;
-    pulse?: boolean;
-    border?: boolean;
-    fixedWidth?: boolean;
-    listItem?: boolean;
-    flip?: FlipProp;
-    size?: SizeProp;
-    pull?: PullProp;
-    rotate?: RotateProp;
-    transform?: string | Transform;
-    symbol?: boolean | string;
-    style?: CSSProperties;
+  icon: IconProp;
+  mask?: IconProp;
+  className?: string;
+  spin?: boolean;
+  pulse?: boolean;
+  border?: boolean;
+  fixedWidth?: boolean;
+  listItem?: boolean;
+  flip?: FlipProp;
+  size?: SizeProp;
+  pull?: PullProp;
+  rotate?: RotateProp;
+  transform?: string | Transform;
+  symbol?: boolean | string;
+  style?: CSSProperties;
 }
 export default function FontAwesomeIcon(props: Props): JSX.Element;
-
