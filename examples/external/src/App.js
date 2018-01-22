b8fa26f2d65b4835af3176c9ffb1cc1f115c7e15
Mike Wilkerson <mwilkerson@gmail.com>
2018-01-22 08:55:47 -0800

    Adding types, derived from fontawesome API types (#46)
    * Add TypeScript types, dependent upon the types in the fontawesome API
    package.
    
    * Add example app that uses TypeScript
    
    * Add @types/react package to dev dependencies
    
    * Reformat with prettier
    
    * Moving from default imports to named imports, both in how this component uses the underlying fontawesome API, and in what this component exports to its clients.
    
    * Update documentation to remove default export/import.

--- examples/external/src/App.js
+++ examples/external/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 
 class App extends Component {
