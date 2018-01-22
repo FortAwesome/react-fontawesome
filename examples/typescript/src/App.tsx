9c8f152836c7c4d9b64e9604fc0a686455c6f70c
Mike Wilkerson <mwilkerson@gmail.com>
2018-01-22 14:54:59 -0800

    add more documentation and more verbose example for TypeScript (#55)

--- examples/typescript/src/App.tsx
+++ examples/typescript/src/App.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+
+// Importing types from the API library along with other exports
+import { IconLookup,
+    IconDefinition,
+    findIconDefinition,
+    library } from '@fortawesome/fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faCog } from '@fortawesome/fontawesome-free-solid';
+import { faCoffee } from '@fortawesome/fontawesome-free-solid';
 import './App.css';
+
+// We're only adding faCoffee to the library so we can look it up.
+// And we're only doing that to demonstrate how the API types might be used.
+// This is not a realistic scenario. You wouldn't normally do things in such a round about way.
+// It's really just to demonstrate use of the types.
+library.add(faCoffee);
+const coffeeLookup: IconLookup = { prefix: 'fas', iconName: 'coffee' };
+const coffeeIconDefinition: IconDefinition = findIconDefinition(coffeeLookup);
 
 export class App extends React.Component {
   render() {
@@ -9,10 +23,7 @@ export class App extends React.Component {
       <div className="App">
           <h1>
               <FontAwesomeIcon
-                  icon={faCog}
-                  style={{backgroundColor: 'lightgrey'}}
-                  mask={faCoffee}
-                  transform="shrink-9 up-1 left-1"
+                  icon={coffeeIconDefinition}
               />
           </h1>
       </div>
