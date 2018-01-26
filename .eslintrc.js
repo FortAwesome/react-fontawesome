dece0680cf405c8a0891d9a80fd4ddf93158b772
robmadole <robmadole@gmail.com>
2018-01-26 14:02:36 -0600

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,18 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "standard",
-  "plugins": [
-    "react",
-    "jest"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: 'babel-eslint',
+  extends: 'standard',
+  plugins: ['react', 'jest'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
   },
-  "rules": {
-    "react/jsx-uses-vars": 1,
-    "react/jsx-uses-react": 1
+  rules: {
+    'space-before-function-paren': 0,
+    'react/jsx-uses-vars': 1,
+    'react/jsx-uses-react': 1
   },
-  "env": {
-    "jest": true
+  env: {
+    jest: true
   }
-};
+}
