2c8b5e60105af7c5eebbd90a0a33403755c35836
robmadole <robmadole@gmail.com>
2018-03-19 11:49:01 -0500

    Update React TS example

--- examples/create-react-app-typescript/src/App.tsx
+++ examples/create-react-app-typescript/src/App.tsx
@@ -8,8 +8,25 @@ import {
   library
 } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import './App.css'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+  faCoffee,
+  faCog,
+  faSpinner,
+  faQuoteLeft,
+  faSquare,
+  faCheckSquare
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  fab,
+  faCoffee,
+  faCog,
+  faSpinner,
+  faQuoteLeft,
+  faSquare,
+  faCheckSquare
+)
 
 // We're only adding faCoffee to the library so we can look it up.
 // And we're only doing that to demonstrate how the API types might be used.
@@ -22,10 +39,67 @@ const coffeeIconDefinition: IconDefinition = findIconDefinition(coffeeLookup)
 export class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>
-          <FontAwesomeIcon icon={coffeeIconDefinition} />
-        </h1>
+      <div id="app">
+        <main className="w-100 min-vh-100 bg-gray8 white sans-serif pa6 flex flex-column justify-center items-center">
+          <div className="mw8 center overflow-hidden">
+            <h2 className="tc ttu tracked3 b f2 mt0 mb2 teal0 o-30">
+              react-fontawesome (TypeScript edition)
+            </h2>
+
+            <ul className="list ma0 pa0 flex flex-row flex-wrap teal4">
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon icon={['fab', 'font-awesome']} size="4x" />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon icon={coffeeIconDefinition} size="4x" />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fas', 'cog']}
+                  spin
+                  fixedWidth={false}
+                  size="4x"
+                />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fas', 'spinner']}
+                  pulse
+                  fixedWidth
+                  size="4x"
+                />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fab', 'fort-awesome']}
+                  rotation={90}
+                  size="4x"
+                />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fab', 'internet-explorer']}
+                  flip="both"
+                  size="4x"
+                />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fab', 'fort-awesome']}
+                  inverse
+                  size="4x"
+                />
+              </li>
+              <li className="pv3 ph2 ma0 link grow">
+                <FontAwesomeIcon
+                  icon={['fab', 'font-awesome']}
+                  size="4x"
+                  transform="left-1 rotate-15"
+                />
+              </li>
+            </ul>
+          </div>
+        </main>
       </div>
     )
   }
