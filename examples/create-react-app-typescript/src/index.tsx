a4084b5f9f19d32e87c0baace1b1c2451c5cbba7
robmadole <robmadole@gmail.com>
2018-12-18 12:48:04 -0600

    Update typescript example

--- examples/create-react-app-typescript/src/index.tsx
+++ examples/create-react-app-typescript/src/index.tsx
@@ -1,8 +1,12 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
