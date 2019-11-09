58c88361d00b6dd3c72d47eee163350cfdd44980
Travis Baker <baker.travis.w@gmail.com>
2019-11-08 23:07:52 -0600

    fix: add 0 to valid rotation props

--- src/components/FontAwesomeIcon.js
+++ src/components/FontAwesomeIcon.js
@@ -79,7 +79,7 @@ FontAwesomeIcon.propTypes = {
 
   pulse: PropTypes.bool,
 
-  rotation: PropTypes.oneOf([90, 180, 270]),
+  rotation: PropTypes.oneOf([0, 90, 180, 270]),
 
   size: PropTypes.oneOf([
     'lg',
