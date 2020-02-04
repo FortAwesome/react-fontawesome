58fc0833e718f75d1723e2faefd29086b43ff334
Anwar Hussain <anwar828@gmail.com>
2020-02-04 14:03:02 +0300

    refactor: using `isObject` util func

--- src/utils/normalize-icon-args.js
+++ src/utils/normalize-icon-args.js
@@ -1,3 +1,5 @@
+import isObject from './is-object';
+
 // Normalize icon arguments
 export default function normalizeIconArgs(icon) {
   // if the icon is null, there's nothing to do
@@ -5,15 +7,15 @@ export default function normalizeIconArgs(icon) {
     return null
   }
   
+  // if the icon is an object and has a prefix and an icon name, return it
+  if (isObject(icon) && icon.prefix && icon.iconName) {
+    return icon
+  }
+  
   // if it's an array with length of two
   if (Array.isArray(icon) && icon.length === 2) {
     // use the first item as prefix, second as icon name
     return { prefix: icon[0], iconName: icon[1] }
-  }
-
-  // if the icon is an object and has a prefix and an icon name, return it
-  if (typeof icon === 'object' && icon.prefix && icon.iconName) {
-    return icon
   }
 
   // if it's a string, use it as the icon name
