34953529634ff79c4e0fd1d8157ef3045e966f5c
Anwar Hussain <anwar828@gmail.com>
2020-02-04 13:53:01 +0300

    fix: swap object and array type checking

--- src/utils/normalize-icon-args.js
+++ src/utils/normalize-icon-args.js
@@ -4,16 +4,16 @@ export default function normalizeIconArgs(icon) {
   if (icon === null) {
     return null
   }
-
-  // if the icon is an object and has a prefix and an icon name, return it
-  if (typeof icon === 'object' && icon.prefix && icon.iconName) {
-    return icon
-  }
-
+  
   // if it's an array with length of two
   if (Array.isArray(icon) && icon.length === 2) {
     // use the first item as prefix, second as icon name
     return { prefix: icon[0], iconName: icon[1] }
+  }
+
+  // if the icon is an object and has a prefix and an icon name, return it
+  if (typeof icon === 'object' && icon.prefix && icon.iconName) {
+    return icon
   }
 
   // if it's a string, use it as the icon name
