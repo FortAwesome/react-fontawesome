eddb45bd4328c5d827e0b7edfb9bf81cf9bc8ec9
Jason Lundien <50338269+jasonlundien@users.noreply.github.com>
2020-12-22 14:13:30 -0600

    Support some new Font Awesome 6 feature (#398)

--- src/components/__tests__/FontAwesomeIcon.test.js
+++ src/components/__tests__/FontAwesomeIcon.test.js
@@ -1,43 +1,58 @@
 import * as fontawesome from '@fortawesome/fontawesome-svg-core'
-import FontAwesomeIcon from '../FontAwesomeIcon'
-import React from 'react'
-import renderer from 'react-test-renderer'
 import log from '../../logger'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faCircle, faSpartan } from '../__fixtures__/icons'
+import { coreHasFeature, REFERENCE_ICON_BY_STYLE, ICON_ALIASES, mount } from '../__fixtures__/helpers'
 
 jest.mock('../../logger')
 
-const faCoffee = {
-  prefix: 'fas',
-  iconName: 'coffee',
-  icon: [
-    640,
-    512,
-    [],
-    'f0f4',
-    'M192 384h192c53 0 96-43 96-96h32c70.6 0 128-57.4 128-128S582.6 32 512 32H120c-13.3 0-24 10.7-24 24v232c0 53 43 96 96 96zM512 96c35.3 0 64 28.7 64 64s-28.7 64-64 64h-32V96h32zm47.7 384H48.3c-47.6 0-61-64-36-64h583.3c25 0 11.8 64-35.9 64z'
-  ]
-}
+beforeEach(() => {
+  fontawesome.library.add(faCoffee, faCircle, faSpartan)
+})
 
-const faCircle = {
-  prefix: 'fas',
-  iconName: 'circle',
-  icon: [
-    640,
-    512,
-    [],
-    'f0f4',
-    'M192 384h192c53 0 96-43 96-96h32c70.6 0 128-57.4 128-128S582.6 32 512 32H120c-13.3 0-24 10.7-24 24v232c0 53 43 96 96 96zM512 96c35.3 0 64 28.7 64 64s-28.7 64-64 64h-32V96h32zm47.7 384H48.3c-47.6 0-61-64-36-64h583.3c25 0 11.8 64-35.9 64z'
-  ]
-}
-
-fontawesome.library.add(faCoffee, faCircle)
-
-function mount(props = {}, { createNodeMock } = {}) {
-  const component = renderer.create(<FontAwesomeIcon {...props} />, {
-    createNodeMock
+test('using a FAT icon using array format', () => {
+  const vm = mount({
+    icon: ['fat', 'spartan']
   })
 
-  return component.toJSON()
+  expect(vm.type).toBe('svg')
+  expect(vm.props.className.includes('fa-spartan')).toBeTruthy()
+})
+
+if (coreHasFeature(ICON_ALIASES)) {
+  test('find a free-solid-svg-icon with array format', () => {
+    fontawesome.library.reset()
+    fontawesome.library.add(faClose)
+    const vm = mount({ icon: ['fas', 'xmark'] })
+
+    expect(vm.type).toBe('svg')
+    expect(vm.props.className.includes('fa-xmark')).toBeTruthy()
+  })
+
+  test('find a free-solid-svg-icon that is an alias ', () => {
+    fontawesome.library.reset()
+    fontawesome.library.add(faClose)
+    const vm = mount({ icon: ['fas', 'close'] })
+
+    expect(vm.type).toBe('svg')
+    expect(vm.props.className.includes('fa-close')).toBeTruthy()
+  })
+}
+
+if (coreHasFeature(REFERENCE_ICON_BY_STYLE)) {
+  test('find a THIN icon with array format', () => {
+    const vm = mount({ icon: ['thin', 'spartan'] })
+
+    expect(vm.type).toBe('svg')
+    expect(vm.props.className.includes('fa-spartan')).toBeTruthy()
+  })
+
+  test('find a FA-THIN icon with array format', () => {
+    const vm = mount({ icon: ['fa-thin', 'spartan'] })
+
+    expect(vm.type).toBe('svg')
+    expect(vm.props.className.includes('fa-spartan')).toBeTruthy()
+  })
 }
 
 test('using pack and name', () => {
