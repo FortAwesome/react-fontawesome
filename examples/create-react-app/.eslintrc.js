c28e82b95da33126e69b2b5d3d6ba9d2350ef3f3
robmadole <robmadole@gmail.com>
2022-05-31 15:56:04 -0500

    Deps updates

---
+++ examples/create-react-app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
